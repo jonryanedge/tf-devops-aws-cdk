f41cbfa4572c7f75ca08fa0e5ec5e938b584a7e3
Jon Edgeworth <edgeworthjr@gmail.com>
2020-12-08 18:12:01 -0500

    Initial commit

---
+++ bin/terraform-pipeline.ts
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from '@aws-cdk/core';
+import { TerraformPipelineStack } from '../lib/terraform-pipeline-stack';
+
+const app = new cdk.App();
+new TerraformPipelineStack(app, 'TerraformPipelineStack');
